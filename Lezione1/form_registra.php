2d614a5f1d64b8338782b3a750f185b6c69bf47a
spyke79 <167540765+spyke79@users.noreply.github.com>
2024-05-10 16:18:23 +0200

    fix

--- Lezione1/form_registra.php
+++ Lezione1/form_registra.php
@@ -5,4 +5,14 @@ echo "<br>nome = ".$_POST['nome'];
 echo "<br>PWD in chiaro: ". $_POST['pwd'];
 echo "<br>PWD Cod: " . md5($_POST['pwd']);
 
+$pwd_md5 = md5($_POST['pwd']);
+
+$sql = 'INSERT INTO utenti 
+        VALUES(null,"'.$_POST['nome'].'","'.$_POST['cognome'].'","'.$pwd_md5.'","'.$_POST['email'].'")';
+
+
+//Vado ad esguire la query
+$conn ->exec($sql);
+echo "dati inseriti";
+
 ?>
