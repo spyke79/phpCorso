c4685cbc7aad4091f46b646cf5af91203c6b13f3
spyke79 <167540765+spyke79@users.noreply.github.com>
2024-05-17 15:32:07 +0200

    table

---
+++ Lezione1/admin/dashboard.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+    //Chiamo la connesisone al DB
+    include('../lib/connDBPDO.php');
+
+    $sql = "SELECT nome, cognome, mail FROM utenti";
+    $ris = $conn->query($sql); //Eseguo la query
+?>
+<div class="container">
+    <table class="table">
+        <tr>
+            <th>Nome</th>
+            <th>Cognome</th>
+            <th>Mail</th>
+        </tr>
+<?php
+    while($row = $ris->fetch()){
+        echo "<tr>";
+        echo "<td>".$row['nome']."</td>";
+        echo "<td>".$row['cognome']."</td>";
+        echo "<td>".$row['mail']."</td>";
+        echo "</tr>";
+    }
+    ?>
+    </table>
+
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- Lezione1/lib/connDBPDO.php
+++ Lezione1/lib/connDBPDO.php
@@ -9,7 +9,7 @@ try{
     $conn = new PDO("mysql:host=$host;dbname=$dbName", $user,$pwd);
     //Vado a vedere eventuali errori
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connessione Riuscita";
+    //echo "Connessione Riuscita";
 }catch(PDOException $e){
     echo "Errore connessione: ". $e->getMessage();
 }
